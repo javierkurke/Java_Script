2e8da9f6f52e736f9d09f7d63fcaaaf43c880415
javierK <javierkurke@gmail.com>
2022-07-02 20:32:59 -0300

    Entrega JavaScript

---
+++ index.js
@@ -0,0 +1,9 @@
+function Persona(nombre,edad,direccion){
+this.nombre=nombre;
+this.edad=edad;
+this.direccion=direccion;
+
+}
+
+const persona1= new Persona("andres",50,"AV.");
+console.log (persona1);
