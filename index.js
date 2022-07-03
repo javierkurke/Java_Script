274d70dfe3c4c16bab628da0acbbfc3b98074fad
javierK <javierkurke@gmail.com>
2022-07-02 23:06:07 -0300

    modificacion

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
+let opcion = 0;
+while (opcion != 4) {
+    let prestamo = 0;
+    let nuevoprestamo = 0;
+    alert("Bienvenido al sistema de Creditos Amapola. Por favor ingrese la opcion deseada.");
+    opcion = parseInt(prompt("1. Solicitar prestamo\n" + "2.Estado actual\n" + "3.Pagar Prestamo\n" + "4.Salir"));
 
-class Item{
-constructor(id,tipo,nombre,imagen,descripcion,precio,cantidad,disponibilidad){
-this.id=id;
-this.tipo=tipo;
-this.nombre=nombre;
-this.imagen=imagen;
-this.descripcion=descripcion;
-this.precio=precio;
-this.cantidad=cantidad;
-this.disponibilidad=disponibilidad;
-}
-darDeBaja(){
-    this.cantidad=false;
-}
-cambiarImagen(imagen){
-    this.imagen=imagen;
-}
-cambiarDescripcion(descripcion){
-    this.descripcion=descripcion;
+     switch (opcion) {
+        case 1:
+            fnuevoprestamo();            
+            break;
+            
+        case 2:
+            imprimirPrestamo(prestamo);           
+            break;
+        case 3:
+            montoAPagar(prestamo);
+            break;
+        case 4:
+            alert("Adios!!!");
+            break;
 
-}
-cambiarPrecio(precio){
-    this.precio=precio;
-}
-
-
+        default:
+            break;
+    }
 
 
+
 }
+
+
+function fnuevoprestamo(){
+    nuevoprestamo = parseInt(prompt("Ingrese el monto deseado. Debe ser menor a $30.000"));
+    if (prestamo + nuevoprestamo > 30000) {
+        alert("Su prestamo no es posible.");
+    } else {
+        let validar = parseInt(prompt("Usted ingreso...$" + nuevoprestamo + ". Presione 1 PARA CONFIRMAR O 0 PARA CANCELAR"));
+        if (validar == 1) {
+            prestamo = prestamo + nuevoprestamo;
+            alert("Su saldo a deber es de $" + prestamo);
+        } else {
+            alert("adios!");
+         }
+    }
     
+}
+function imprimirPrestamo(prestamo){
+    alert("Su saldo a deber es ...$" + prestamo);
+
+}
+function montoAPagar(prestamo) {
+    {
+        let pago = prompt("Ingrese el monto que desee pagar...");
+        if (pago > prestamo) {
+            alert("INGRESO MAS DINERO DEL QUE DEBIA, POR FAVOR INGRESE NUEVAMENTE");
+        }
+    } while (pago > prestamo);
+
+    prestamo == prestamo - pago;
+    alert("Su saldo actual es... $" + prestamo);
+    
+}
+
+
