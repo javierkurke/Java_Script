7ebbb0a5f44aa558d9bd2fd848b634e24d47cefb
javierK <javierkurke@gmail.com>
2022-07-23 18:22:59 -0300

    Ultima entrega

--- index.js
+++ index.js
@@ -1,8 +1,80 @@
-let a1=document.getElementById("a1");
-let a2=document.getElementById("a2");
-let a3=document.getElementById("a3");
-let a4=document.getElementById("a4");
-let a0=document.getElementById("a0");
+
+class Item {
+    constructor(tipo, nombre, imagen, descripcion, precio, cantidad) {
+        this.id = Math.round(Math.random() * 100);
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.alta = false;
+        this.fechaAlta = new Date();
+        this.SugerenciaDelCliente = null;
+    }
+    cambiarImagen(imagen1) {
+        imagen = imagen1;
+    }
+    cambiarDescripcion(descripcion2) {
+        descripcion = descripcion2;
+
+    }
+    cambiarPrecio(precio2) {
+        precio = precio2;
+    }
+    cambiarCantidad(cantidad2) {
+        cantidad = cantidad2;
+
+    }
+    ingresarSujerencia(SujerenciaDelCliente){
+    SugerenciaDelCliente=SujerenciaDelCliente;
+    }
+}
+function cargarProductos(){
+    productos.push(new Item("Bebida","Cafe",`"assets/img/cafe.jpeg"`," Cafe, Cortado o Cafe Doble",110,100))
+    productos.push(new Item("Bebida","Gaseosa",`"assets/img/gaseosachico.png"`,"Linea Coca-cola. Consultar disponibilidad",110,100))
+    productos.push(new Item("Comida","Hamburguesa Clasica",`"assets/img/hambuerguesa1chico.png"`,"Lechaga, tomate, queso, morrones y jamon.",110,100))
+    productos.push(new Item("Comida","Hamburguesa Especial",`"assets/img/hamburguesa2chico.png"`,"Jamon, Queso, Morrones, Champignones.",110,100))
+    productos.push(new Item("Comida","Pizzanesa",`"assets/img/pizzas1.jpg"`,"Milanesas Gratinadas con Queso Cheddar y papas",110,100))
+    productos.push(new Item("Comida","Picada clasica",`"assets/img/picada2.jpg"`," Picada completa.",110,100))
+    productos.push(new Item("Comida","Pizza",`"assets/img/pizzachico.png"`,"Muzzarella ,Especial ,Fugazza o 4-Quesos .",110,100))
+    productos.push(new Item("Bebida","Whisky",`"assets/img/w3.webp"`," Jack Daniels, Chivas Regal o Jonnie Woker",110,100))
+    productos.push(new Item("Postre","Postre",`"assets/img/postre2.jpg"`,"Mouse, Flanes, Paraiso de Frutos Rojos, Helado.",110,100))
+    
+    
+    }
+
+const productos = [];
+const carrito =[];
+cargarProductos();
+let conteiner= document.getElementById("contenedor");
+for (const iterator of productos) {``
+    let parrafo=document.createElement("div");
+    parrafo.innerHTML=`<div class="card text-white bg-dark mb-3 HD" style="max-width: 30rem;">
+    <div class="card-header"><img src=${iterator.imagen} class="card-img-top" alt="..."></div>
+    <div class="card-body p16">
+        <h5 class="card-title p16">${iterator.nombre}</h5>
+        <p class="card-text">${iterator.descripcion}</p>
+    </div>
+    <form class="formulario">
+        <button type="button" id="${iterator.id}1" class="btn btn-primary formulario_boton">-</button>
+        <input type="text" id="${iterator.id}3" class="formulario_texto" value="0">
+        <button type="button" id="${iterator.id}2" class="btn btn-primary formulario_boton ">+</button>
+        <button type="button" id="${iterator.id}4" class="btn btn-success formulario_boton">Agregar al Carrito</button>
+    
+        </form>
+    <br>
+    
+    </div>`;
+    document.getElementById("contenedor").append(parrafo);
+}
+for (const iterator of productos) {
+
+let a1 = document.getElementById(`${iterator.id}1`);
+let a2 = document.getElementById(`${iterator.id}2`);
+let a3 = document.getElementById(`${iterator.id}3`);
+let a4 = document.getElementById(`${iterator.id}4`);
+let a0 = document.getElementById("a0");
 if (isNaN(parseInt(a3.value))) 
 {a3.value=0;
     }
@@ -22,8 +94,25 @@ a4.addEventListener("click", respuestaa3)
 function respuestaa3() {
     if (parseInt(a3.value)>0) {
         a0.innerHTML=parseInt(a0.innerHTML)+parseInt(1);
+        iterator.cantidad=parseInt(a3.value);
+        carrito.push(iterator);
     }
   
 
     
 }
+
+
+
+
+    
+}
+
+    
+  
+
+
+    
+    
+
+
