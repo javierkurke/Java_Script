6ff085d58964dbde48e775d76f7ca9ac7b14b086
javierK <javierkurke@gmail.com>
2022-08-06 17:42:38 -0300

    ultima version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 class Item {
-    constructor(tipo, nombre, imagen, descripcion, precio, cantidad) {
-        this.id = Math.round(Math.random() * 100);
+    constructor(id,tipo, nombre, imagen, descripcion, precio, cantidad) {
+        this.id = id;
         this.tipo = tipo;
         this.nombre = nombre;
         this.imagen = imagen;
@@ -31,17 +31,16 @@ class Item {
         SugerenciaDelCliente = SujerenciaDelCliente;
     }
 }
-
 function cargarProductos() {
-    productos.push(new Item("Bebida", "Cafe", `"assets/img/cafe.jpeg"`, " Cafe, Cortado o Cafe Doble", 100, 100))
-    productos.push(new Item("Bebida", "Gaseosa", `"assets/img/gaseosachico.png"`, "Linea Coca-cola. Consultar disponibilidad", 110, 100))
-    productos.push(new Item("Bebida", "Whisky", `"assets/img/w3.webp"`, " Jack Daniels, Chivas Regal o Jonnie Woker", 110, 100))
-    productos.push(new Item("Comida", "Hamburguesa Clasica", `"assets/img/hambuerguesa1chico.png"`, "Lechaga, tomate, queso, morrones y jamon.", 110, 100))
-    productos.push(new Item("Comida", "Hamburguesa Especial", `"assets/img/hamburguesa2chico.png"`, "Jamon, Queso, Morrones, Champignones.", 110, 100))
-    productos.push(new Item("Comida", "Pizzanesa", `"assets/img/pizzas1.jpg"`, "Milanesas Gratinadas con Queso Cheddar y papas", 110, 100))
-    productos.push(new Item("Comida", "Picada clasica", `"assets/img/picada2.jpg"`, " Picada completa.", 110, 100))
-    productos.push(new Item("Comida", "Pizza", `"assets/img/pizzachico.png"`, "Muzzarella ,Especial ,Fugazza o 4-Quesos .", 110, 100))
-    productos.push(new Item("Postre", "Postre", `"assets/img/postre2.jpg"`, "Mouse, Flanes, Paraiso de Frutos Rojos, Helado.", 110, 100))
+    productos.push(new Item("1","Bebida", "Cafe", `"assets/img/cafe.jpeg"`, " Cafe, Cortado o Cafe Doble", 100, 100))
+    productos.push(new Item("2","Bebida", "Gaseosa", `"assets/img/gaseosachico.png"`, "Linea Coca-cola. Consultar disponibilidad", 110, 100))
+    productos.push(new Item("3","Bebida", "Whisky", `"assets/img/w3.webp"`, " Jack Daniels, Chivas Regal o Jonnie Woker", 110, 100))
+    productos.push(new Item("4","Comida", "Hamburguesa Clasica", `"assets/img/hambuerguesa1chico.png"`, "Lechaga, tomate, queso, morrones y jamon.", 110, 100))
+    productos.push(new Item("5","Comida", "Hamburguesa Especial", `"assets/img/hamburguesa2chico.png"`, "Jamon, Queso, Morrones, Champignones.", 110, 100))
+    productos.push(new Item("6","Comida", "Pizzanesa", `"assets/img/pizzas1.jpg"`, "Milanesas Gratinadas con Queso Cheddar y papas", 110, 100))
+    productos.push(new Item("7","Comida", "Picada clasica", `"assets/img/picada2.jpg"`, "Picada completa", 110, 100))
+    productos.push(new Item("8","Comida", "Pizza", `"assets/img/pizzachico.png"`, "Muzzarella ,Especial ,Fugazza o 4-Quesos .", 110, 100))
+    productos.push(new Item("9","Postre", "Postre", `"assets/img/postre2.jpg"`, "Mouse, Flanes, Paraiso de Frutos Rojos, Helado.", 110, 100))
     for (const iterator of productos) {
     
         let parrafo = document.createElement("div");
@@ -66,33 +65,29 @@ function cargarProductos() {
     }
 
 }
-
 const productos = [];
 const carrito = [];
+const guardarStorage=(clave,valor)=>{sessionStorage.setItem(clave,valor)}
+let traerStorage= JSON.stringify(sessionStorage.getItem(`nombre`));
+let ArrayDelStorage=[];
+ArrayDelStorage=traerStorage;
+
+
+
+
+
 cargarProductos();
+
 let botoncarrito=document.getElementById("boton_carrito");
 botoncarrito.addEventListener("click", aparece);
-
 function aparece(){
     const carrito1= document.getElementById("carrito");
     if (carrito1.className == "carrito_contenido_padre") {
         carrito1.setAttribute("class",`carrito_contenido_padre2`);    
     }else{
         carrito1.setAttribute("class",`carrito_contenido_padre`);
-    }
-  
-    
-    
-    
+    }   
 }
-function desaparece(){
-    const carrito1= document.getElementById("carrito");
-    carrito1.setAttribute("class",`carrito_contenido_padre`);
-
-
-
-}
-
 
 for (const iterator of productos) {
 
@@ -121,18 +116,22 @@ for (const iterator of productos) {
         if (parseInt(a3.value) > 0) {
             const resultado =carrito.indexOf(iterator);
             if (resultado==-1) {
-                console.log(resultado);
+                
                 a0.innerHTML = parseInt(carrito.length+1);
                 iterator.cantidad = parseInt(a3.value);
                 iterator.precio2= parseInt(iterator.cantidad)*parseInt(iterator.precio);
                 carrito.push(iterator);
-                cargarCarrito();                
+                cargarCarrito(); 
+                guardarStorage(iterator.id,JSON.stringify(iterator));
+               
+                
             }                 
           else {
-            console.log(carrito);
+          
             carrito[resultado].cantidad= carrito[resultado].cantidad + parseInt(a3.value);
             carrito[resultado].precio2= carrito[resultado].cantidad*carrito[resultado].precio;            let 
             var1=document.getElementById(iterator.id);
+            guardarStorage( carrito[resultado].id,JSON.stringify(carrito[resultado]))
             var1.innerHTML=`
             <img class="imagen_lista" src=${iterator.imagen} width="10%" height="10%
                 ">
@@ -140,28 +139,33 @@ for (const iterator of productos) {
             <li class="item_contenido">Nombre: ${iterator.nombre}</li>
             <li class="item_contenido">Cantidad: ${ carrito[resultado].cantidad}</li>
             <li class="item_contenido">Precio:$${carrito[resultado].precio2}</li>
-            <button id="e${var1}" class="boton_eliminar" type="button"><lord-icon
+            <button id="e${iterator.id}" class="boton_eliminar" type="button"><lord-icon
                 src="https://cdn.lordicon.com/rivoakkk.json"
                 trigger="hover"
-                colors="primary:#ffffff,secondary:#c71f16"
+                colors="primary:#ffffff,secondary:#c71f16 "
                 style="width:50px;height:50px">
             </lord-icon></button>`;
-            
-            
-           
-            
-          }
+            let e1 = document.getElementById(`e${var1.id}`);
+            e1.addEventListener("click", eliminar2);
+            function eliminar2() {
+                
+                var2=carrito.indexOf(iterator);
+                carrito.splice(var2,1);
+                var1.remove();
+                let a1 = document.getElementById("a0");
+                a1.innerHTML = parseInt(carrito.length);
+                sessionStorage.removeItem(`${iterator.id}`)
+    
+    
+            }
+
+            }
            
         }
-
-
-
-    }
-
-
+       }
     function cargarCarrito() {
         for (let index = carrito.length-1; index < carrito.length; index++) {
-            let var1 = Math.round(Math.random() * 1000);
+            
             
             let parrafo = document.createElement("div");
             parrafo.classList.add("carrito_contenido");
@@ -174,26 +178,28 @@ for (const iterator of productos) {
     <li class="item_contenido">Nombre: ${iterator.nombre}</li>
     <li class="item_contenido">Cantidad: ${iterator.cantidad}</li>
     <li class="item_contenido">Precio:$${iterator.precio2}</li>
-    <button id="e${var1}" class="boton_eliminar" type="button"><lord-icon
+    <button id="e${iterator.id}" class="boton_eliminar" type="button"><lord-icon
         src="https://cdn.lordicon.com/rivoakkk.json"
         trigger="hover"
         colors="primary:#ffffff,secondary:#c71f16"
         style="width:50px;height:50px">
     </lord-icon></button>`;
             document.getElementById("carrito").append(parrafo);
-            for (const iterator of carrito) {
-                let e1 = document.getElementById(`e${var1}`);
+           
+                let e1 = document.getElementById(`e${iterator.id}`);
                 e1.addEventListener("click", eliminar);
                 function eliminar() {
-                    carrito.splice(index,1);
+                    var2=carrito.indexOf(iterator);
+                    carrito.splice(var2,1);
                     parrafo.remove();
                     let a0 = document.getElementById("a0");
                     a0.innerHTML = parseInt(carrito.length);
-        
+                    sessionStorage.removeItem(`${iterator.id}`)
+                    
         
                 }
         
-            } 
+           
 
         }
 
